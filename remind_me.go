03b8284816dd922abd6c19b60cc8ea0807c54767
davidfloyd91 <davidfloyd91@gmail.com>
2019-06-19 10:45:03 -0400

    get events route

--- remind_me.go
+++ remind_me.go
@@ -21,9 +21,13 @@ type Event struct {
 }
 
 var db *gorm.DB
+// right? wrong?
 var err error
 
 func main() {
+    /******************************
+        initialize the database
+    ******************************/
     host := "localhost"
     port := 5432
     user := "postgres"
@@ -34,15 +38,18 @@ func main() {
 
     connectionString := fmt.Sprintf(t, host, port, user, dbname, sslmode)
 
+    // err????????
     db, err = gorm.Open("postgres", connectionString)
 
     db.AutoMigrate(&Event{})
     defer db.Close()
 
+    // err????????
     if err != nil {
         fmt.Println("Error in postgres connection: ", err)
     }
 
+    // err????????
     err = db.DB().Ping()
 
     if err != nil {
@@ -50,18 +57,30 @@ func main() {
     }
 
     fmt.Println("Successfully connected!")
+    /******************************
+        .............. did that
+    ******************************/
 
     initRouter()
 }
 
 var Home = func(w http.ResponseWriter, r *http.Request) {
+    // error handling here (not all that important)
     w.Header().Add("Content-Type", "application/json")
     json.NewEncoder(w).Encode("Welcome home!")
+}
+
+var GetEvents = func(w http.ResponseWriter, r *http.Request) {
+    var events []Event
+    db.Find(&events)
+    w.Header().Add("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(&events)
 }
 
 var CreateEvent = func(w http.ResponseWriter, r *http.Request) {
     event := &Event{}
 
+    // should it be declaring a new err?
     err := json.NewDecoder(r.Body).Decode(event)
     if err != nil {
         w.WriteHeader(http.StatusBadRequest)
@@ -70,24 +89,27 @@ var CreateEvent = func(w http.ResponseWriter, r *http.Request) {
 
     db.Create(&event)
 
-    // w.Header().Add("Content-Type", "application/json")
-    // json.NewEncoder(w).Encode(event)
+    // error handling here
+    w.Header().Add("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(event)
 }
 
 func initRouter() {
     router := mux.NewRouter()
 
     router.HandleFunc("/", Home)
+    router.HandleFunc("/events", GetEvents).Methods("GET")
     router.HandleFunc("/events", CreateEvent).Methods("POST")
 
     // router.Use(JwtAuthentication)
 
     handler := cors.Default().Handler(router)
 
+    // declare new err here?
     err := http.ListenAndServe(":8000", handler)
+
+    // which err is this even referring to?
 	  if err != nil {
 		    fmt.Print(err)
     }
 }
-
-// https://github.com/Sach97/pgsearch-gorm-example/blob/master/search/main.go
