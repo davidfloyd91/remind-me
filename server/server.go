833efb7798142723400fd19afe415f7100ff8885
davidfloyd91 <davidfloyd91@gmail.com>
2019-08-04 02:12:34 -0400

    Add delete users -- set deleted_at, no deletion or filter

--- server/server.go
+++ server/server.go
@@ -1,12 +1,12 @@
 package server
 
 import (
-  "database/sql"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-  "strings"
+	"strings"
 	"time"
 
 	"github.com/davidfloyd91/remind-me/db"
@@ -25,57 +25,57 @@ func Start() {
 
 // https://stackoverflow.com/questions/40266633/golang-insert-null-into-sql-instead-of-empty-string
 func newNullString(s string) sql.NullString {
-    if len(s) == 0 {
-        return sql.NullString{}
-    }
-    return sql.NullString{
-         String: s,
-         Valid: true,
-    }
+	if len(s) == 0 {
+		return sql.NullString{}
+	}
+	return sql.NullString{
+		String: s,
+		Valid:  true,
+	}
 }
 
 // $ curl http://localhost:8000/ -v
 var root = http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
-  json.NewEncoder(w).Encode("Hello, World!")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode("Hello, World!")
 })
 
 var users = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-  var users []types.User
-  var rows *sql.Rows
+	var users []types.User
+	var rows *sql.Rows
 
-  // definitely a better method in net/url
-  paramId := strings.Split(r.URL.String(), "/")[2]
+	// definitely a better method in net/url
+	paramId := strings.Split(r.URL.String(), "/")[2]
 
 	switch r.Method {
 	case "GET":
-    // $ curl http://localhost:8000/users/ -v
-    if paramId == "" {
-  		query := `
+		// $ curl http://localhost:8000/users/ -v
+		if paramId == "" {
+			query := `
           SELECT id, username, email, created_at, updated_at, deleted_at
           FROM users
         `
 
-      var err error
-  		rows, err = db.DB.Query(query)
-  		if err != nil {
-  			panic(err)
-  		}
+			var err error
+			rows, err = db.DB.Query(query)
+			if err != nil {
+				panic(err)
+			}
 
-    // $ curl http://localhost:8000/users/3 -v
-    } else {
-      query := `
+			// $ curl http://localhost:8000/users/3 -v
+		} else {
+			query := `
           SELECT id, username, email, created_at, updated_at, deleted_at
           FROM users
           WHERE id = $1
         `
 
-      var err error
-      rows, err = db.DB.Query(query, paramId)
-      if err != nil {
-  			panic(err)
-  		}
-    }
+			var err error
+			rows, err = db.DB.Query(query, paramId)
+			if err != nil {
+				panic(err)
+			}
+		}
 
 	// $ curl http://localhost:8000/users/ -d '{"Username":"Alice","Email":"alice@alice.alice", "Password":"lol"}' -v
 	case "POST":
@@ -86,7 +86,7 @@ var users = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusBadRequest)
 		}
 
-    // passwords are not being hashed -- must fix
+		// passwords are not being hashed -- must fix
 		query := `
         INSERT INTO users (username, email, password)
         VALUES ($1, $2, $3)
@@ -98,20 +98,20 @@ var users = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			panic(err)
 		}
 
-  // curl -X PATCH http://localhost:8000/users/1 -d '{"Password":"Superhilar"}' -v
-  case "PATCH":
-    if paramId == "" {
-      return
-    }
+	// curl -X PATCH http://localhost:8000/users/1 -d '{"Username":"Egh","Password":"Superhilar"}' -v
+	case "PATCH":
+		if paramId == "" {
+			return
+		}
 
-    user := &types.User{}
+		user := &types.User{}
 
-    err := json.NewDecoder(r.Body).Decode(user)
-    if err != nil {
-      w.WriteHeader(http.StatusBadRequest)
-    }
+		err := json.NewDecoder(r.Body).Decode(user)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+		}
 
-    query := `
+		query := `
         UPDATE users SET
           username = COALESCE($1, username),
           email = COALESCE($2, email),
@@ -120,32 +120,52 @@ var users = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         RETURNING id, username, email, created_at, updated_at, deleted_at
       `
 
-    rows, err = db.DB.Query(query, newNullString(user.Username), newNullString(user.Email), newNullString(user.Password), paramId)
-    if err != nil {
-      panic(err)
-    }
+		rows, err = db.DB.Query(query, newNullString(user.Username), newNullString(user.Email), newNullString(user.Password), paramId)
+		if err != nil {
+			panic(err)
+		}
+
+		// nothing is keeping us from seeing deleted users so far
+	// curl -X DELETE http://localhost:8000/users/2 -v
+	case "DELETE":
+		if paramId == "" {
+			return
+		}
+
+		query := `
+        UPDATE users SET
+          deleted_at = current_timestamp
+        WHERE id = $1
+        RETURNING id, username, email, created_at, updated_at, deleted_at
+      `
+
+		var err error
+		rows, err = db.DB.Query(query, paramId)
+		if err != nil {
+			panic(err)
+		}
 	} // close switch
 
-  for rows.Next() {
-    var Id uint
-    var Username string
-    var Email string
-    var CreatedAt time.Time
-    var UpdatedAt time.Time
-    var DeletedAt time.Time
+	for rows.Next() {
+		var Id uint
+		var Username string
+		var Email string
+		var CreatedAt time.Time
+		var UpdatedAt time.Time
+		var DeletedAt time.Time
 
-    rows.Scan(&Id, &Username, &Email, &CreatedAt, &UpdatedAt, &DeletedAt)
+		rows.Scan(&Id, &Username, &Email, &CreatedAt, &UpdatedAt, &DeletedAt)
 
-    users = append(users, types.User{
-      Id:        Id,
-      Username:  Username,
-      Email:     Email,
-      CreatedAt: CreatedAt,
-      UpdatedAt: UpdatedAt,
-      DeletedAt: DeletedAt,
-    })
-  }
+		users = append(users, types.User{
+			Id:        Id,
+			Username:  Username,
+			Email:     Email,
+			CreatedAt: CreatedAt,
+			UpdatedAt: UpdatedAt,
+			DeletedAt: DeletedAt,
+		})
+	}
 
-  w.Header().Set("Content-Type", "application/json")
-  json.NewEncoder(w).Encode(users)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
 }) // close users
